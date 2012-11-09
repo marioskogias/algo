80e383841515eae50e36c61fb80dbbdec641580d
marioskogias <marioskogias@gmail.com>
2012-11-09 22:37:03 +0200

    Δες στο τέλος τι πρέπει να προσθέσεις. Αφού εντόπισες τη θέση με το μέγιστο άθροισμα προσθέτεις αριστερά και δεξιά

--- vocation.c
+++ vocation.c
@@ -7,16 +7,17 @@ int main () {
 	int * days;
 	int maxSum,maxStartIndex,maxEndIndex,n,aveTemp,count;
 
+	
+
 	maxStartIndex = 0;
 	maxEndIndex = 0;
 	maxSum = -214783647;
 	scanf("%d %d",&n,&aveTemp);
 
 	days = malloc(n*sizeof(int));
-	
 	for (count=0;count<n;count++) {
 		scanf("%d",&days[count]);
-		//days[count] = days[count] - aveTemp;
+		days[count] = days[count] - aveTemp;
 	} 
 		
 	
@@ -25,6 +26,7 @@ int main () {
 	int currentEndIndex;
 	for (currentEndIndex=1;currentEndIndex<=n;currentEndIndex++) {
 		currentMaxSum = currentMaxSum + days[currentEndIndex];
+
 		
 		if (currentMaxSum > maxSum) {
 			maxSum=currentMaxSum;
@@ -36,10 +38,27 @@ int main () {
             currentMaxSum = 0;
             currentStartIndex = currentEndIndex + 1 ;
 		}
+
+    	
+	}
+
+	while ((maxSum>=0) && (maxEndIndex<n) && (maxStartIndex>=0)){
+		if (days[maxStartIndex-1]>days[maxEndIndex+1]) {
+				maxStartIndex = maxStartIndex-1;
+				maxSum = maxSum + days[maxStartIndex];
+		}
+		else {
+			maxEndIndex = maxEndIndex+1;
+			maxSum = maxSum + days[maxEndIndex];
+		}
+
+		
+			
 	}
 	
+	int res = maxEndIndex - maxStartIndex - 1;
 
-	printf("%d %d\n", maxEndIndex, maxStartIndex);
-	
+	printf("%d %d %d\n", maxStartIndex, maxEndIndex, maxSum);
+	//printf("%d\n", res);
 
 }
