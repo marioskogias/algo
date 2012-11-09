761f21d5c54ab1df0b53818c3015587faf53be94
marioskogias <marioskogias@gmail.com>
2012-11-09 20:44:24 +0200

    Added a second slow, maybe better version

--- vocation.c
+++ vocation.c
@@ -11,18 +11,20 @@ int main () {
 
 	days = malloc(n*sizeof(int));
 	
-	for (count=0;count<n;count++) 
+	for (count=0;count<n;count++) {
 		scanf("%d",&days[count]);
+		days[count] = days[count] - aveTemp;
+	} 
+		
 	
 	curent = 0;
-
+	curent = -1;
 	while(curent < n) {
 		count = 1;
 		psum = 0;
 		while ((curent+count) < n) {
 			psum = psum + days[curent+count];
-			av = psum / count;
-			if ((av >= aveTemp) && (max < count)) max = count;
+			if ((psum >= 0) && (max < count)) max = count;
 			count++;
 		}
 		curent++;	
