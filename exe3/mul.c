73b292eac63603a48a8a3f72fd88b0b4a715963a
marioskogias <marioskogias@gmail.com>
2013-01-21 18:18:24 +0200

    ready power, hope so...

--- exe3/mul.c
+++ exe3/mul.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void matrixMul(int * a, int * b, int * res, int size) {
+void matrixMul(int * a, int * b, int * res, int size) { // η res πρέπει να είναι αρχικοποιημένη
 	int i,j,k,temp1, temp2;
 	 
 	for (i=0;i<size;i++) {
@@ -29,43 +29,67 @@ int fastpower(int a,int n) {
 	return result;	
 }
 
-int * fastPowerMatrix(int *a,int * res, int size,int power) {  // το res είναι μοναδιαίος πίνακας
+int * fastPowerMatrix(int *a,int size,int power) { 
 	
+	int i;
+	int * temp;
+	int *tempTable = calloc(size*size,sizeof(int));  //αρχικοποίηση σε μοναδιαίο πίνακα
+	for (i=0;i<size;i++)
+		*(tempTable+i*size+i)=1;
 
+	int * res = calloc(size*size,sizeof(int));  //αρχικοποίηση σε μοναδιαίο πίνακα
+	for (i=0;i<size;i++)
+		*(res+i*size+i)=1;
+	
 	while(power>0) {
 		if(power&1) {
-			temp = matrixMul(a,res,size);
-			//free(res);
-			res = temp;
+			matrixMul(a,res,tempTable,size);
+			temp = res;
+			res = tempTable;
+			tempTable = temp;
 		}
 			
 		power = power>>1;
-		temp = matrixMul(a,a,size);
-		free(a);
-		a = temp;
-
+		matrixMul(a,a,tempTable,size);
+		temp = a;
+		a = tempTable;
+		tempTable = a;
 	}
+	
+
 	return res;
+	
 
 }
 
 int main() {
-	int i;
+	/*int i;
 	int a[4];
 	a[0] = 1;
 	a[1] = 2;
 	a[2] = 3;
 	a[3] = 4;
-	int * c =  malloc(4*sizeof(int)) ;
+	int * c;
 	
-	matrixMul(a,a,c,2);
-		
-	//c =  fastPowerMatrix(a,2,2);
-	//printf("----------\n");
-	for (i=0;i<4;i++)
-		printf("%d\n",*(c+i));
+	
 
-	//printf("%d\n",fastpower(2,10));
+	
+	
+	
+	int * d = malloc(4*sizeof(int));
+
+	matrixMul(a,a,d,2);
+	for (i=0;i<4;i++)
+		printf("%d\n",*(d+i));	
+
+	c = fastPowerMatrix(a,2,2);
+
+	
+	printf("the last\n");
+	for (i=0;i<4;i++)
+		printf("%d\n",*(c+i));*/
+
+
 	
 
 }
