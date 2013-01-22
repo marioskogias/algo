0e74d51f5aafde574a29e8ca30d5001b959ddcb9
marioskogias <marioskogias@gmail.com>
2013-01-22 19:16:25 +0200

    union find seems to be working

--- exe3/testcases/testcases.c
+++ exe3/testcases/testcases.c
@@ -5,11 +5,11 @@ struct node {
 	int size;
 } ;
 
-int find (int x, struct node * A) {
+/*int find (int x, struct node * A) {
 	while (x != A[x].parent)
 		x = A[x].parent;
 	return x ; 
-}
+}*/
 
 void unionTree(int x, int y, struct node * A) {
 	if (x == y) return;
@@ -25,5 +25,30 @@ int findTreePathCompression(int x, struct node * A) {
 } 
 
 int main() {
+
+	struct node a[7];
+	a[0].parent = 0;
+	a[0].size = 7;
+
+	a[1].parent = 0;
+	a[1].size = 0;
+
+	a[2].parent = 0;
+	a[2].size = 0;
+
+	a[3].parent = 2;
+	a[3].size = 0;
+
+	a[4].parent = 3;
+	a[4].size = 0;
+
+	a[5].parent = 4;
+	a[5].size = 0;
+
+	a[6].parent = 4;
+	a[6].size = 0;
+
+	printf(" the parent is %d\n", findTreePathCompression(5,a));
+
 
 }
