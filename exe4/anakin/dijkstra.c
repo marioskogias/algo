4e958cfc7e3279bca0ddc870d33ea90e691a4bf3
marioskogias <marioskogias@gmail.com>
2013-03-03 19:29:13 +0200

    moving on

--- exe4/anakin/dijkstra.c
+++ exe4/anakin/dijkstra.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "stdlib.h"
 
 struct node {
 	int name;
@@ -25,21 +26,40 @@ void swap(struct node q1,struct node q2) {
 }
 
 void DecreaseKey(struct node * Q,int x,int k){
-	int i=0;
-	while (i<2*n) {
-		if (Q[i].name == x) 
-			break;
-		else
-			i++; 
-	}
+	int i = x;
 	Q[i].distance = k;
 	while ((i/2 != 0) && (k<Q[i/2].distance)){
 		swap(Q[i],Q[i/2]);
 		i=i/2;
 	}
 }
+
+void IncreaseKey(struct node * Q,int x,int k){
 	
-		
+	Q[x].distance = k;
+
+	while ((2*x)<(2*n)) {
+		if (Q[2*x].distance < Q[2*x+1].distance){
+			swap(Q[x],Q[2*x]);
+			x=2*x;
+		} else {
+			swap(Q[x],Q[2*x+1]);
+			x=2*x+1;
+		}
+	}
+}
+
+
+struct node * ExtractMin(struct node * Q){
+	struct node * p = malloc(sizeof(struct node));
+	p->name = Q->name;
+	p->distance = Q->distance;
+	swap(Q[0],Q[n-1]);	
+	n = n-1;
+	IncreaseKey(Q, 1, Q[0].distance);
+	return p;		
+}
+	
 
 
 
