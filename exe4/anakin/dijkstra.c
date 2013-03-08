6fcddde221beaa49711646de7813f767612ea5cd
marioskogias <marioskogias@gmail.com>
2013-03-08 13:50:21 +0200

    maybe correct binary heap dijkstra

--- exe4/anakin/dijkstra.c
+++ exe4/anakin/dijkstra.c
@@ -12,35 +12,42 @@ struct edge {
 	int length;
 };
 
-int n;
+int n=0;
 int * keys;
+int * place;
 
-struct node *  EmptyQ() {
+struct node *  EmptyQ(int count) {
 	n = 0;
-	struct node * q = malloc(n*sizeof(struct node));
+	struct node * q = calloc(2*count,sizeof(struct node));
 	return q;
 }
 
 
 
-void swap(struct node q1,struct node q2) {
-	int na = q1.name;
-	int dis = q1.distance;
+void swap(struct node * q1,struct node * q2) {
+	int na = q1->name;
+	int dis = q1->distance;
+	int p;
 
-	q1.name = q2.name;
-	q1.distance = q2.distance;
+	p = place[q1->name];
+	place[q1->name] = place[q2->name];
+	place[q2->name] = p;
 
-	q2.name = na;
-	q2.distance = dis;
+	q1->name = q2->name;
+	q1->distance = q2->distance;
+
+	q2->name = na;
+	q2->distance = dis;
+
 
 }
 
 void DecreaseKey(struct node * Q,int x,int k){
 	int i = x;
 	Q[i].distance = k;
-	keys[i] = k;
+	keys[Q[i].name] = k;
 	while ((i/2 != 0) && (k<Q[i/2].distance)){
-		swap(Q[i],Q[i/2]);
+		swap(Q+i,Q+i/2);
 		i=i/2;
 	}
 }
@@ -48,14 +55,14 @@ void DecreaseKey(struct node * Q,int x,int k){
 void IncreaseKey(struct node * Q,int x,int k){
 	
 	Q[x].distance = k;
-	keys[x] = k;
+	keys[Q[x].name] = k;
 
-	while ((2*x)<(2*n)) {
+	while ((2*x)<n) { // while x not leaf
 		if (Q[2*x].distance < Q[2*x+1].distance){
-			swap(Q[x],Q[2*x]);
+			swap(Q+x,Q+2*x);
 			x=2*x;
 		} else {
-			swap(Q[x],Q[2*x+1]);
+			swap(Q+x,Q+2*x+1);
 			x=2*x+1;
 		}
 	}
@@ -66,72 +73,113 @@ void Insert(struct node *Q, int x,int k) {
 	Q[n].name = x;
 	Q[n].distance = k;
 	keys[x] = k;
-	DecreaseKey(Q, x, k);	
+	place[x] = n;
+	DecreaseKey(Q, x, k);
+	printf("insertion\n");
 }
 
 
 
 struct node * ExtractMin(struct node * Q){
 	struct node * p = malloc(sizeof(struct node));
-	p->name = Q->name;
-	p->distance = Q->distance;
-	swap(Q[0],Q[n-1]);
-	free(Q+n-1);	// mallon tha vgei
+	p->name = Q[1].name;
+	p->distance = Q[1].distance;
+	swap(Q+1,Q+n);
+	//free(Q+n-1);	// mallon tha vgei
 	n = n-1;
-	IncreaseKey(Q, 1, Q[0].distance);
+	IncreaseKey(Q, 1, Q[1].distance);
 	return p;		
 }
 	
 
+void insertEdge(struct edge ** e,int h,int t,int d) {
+		struct edge * temp1;
+		struct edge * temp2;
+		struct edge * temp = malloc(sizeof(struct edge));
+		
+		temp->next = NULL;
+		temp->tail = t;
+		temp->length = d;
+		temp1 = e[h];
+		
+		if (temp1 == NULL) {
+			printf("is NULL\n");
+			e[h] = temp;
+		}
+		else {
+			temp2 = temp1;
+			while(temp1 != NULL) {
+				temp2 = temp1;
+				temp1 = temp1->next;
+			}
+			temp2->next = temp;	
+		}
+
+
+}
 
 
 
 int main() {
-	int m,k,l,b,i;
-	scanf("%d %d %d %d %d",&n,&m,&k,&l,&b);
-	int * D = malloc(n*sizeof(int));
-	keys = malloc(n*sizeof(int));
-	struct node * Q = EmptyQ();
+	int m,k,l,b,i,nodeNo;
+	//scanf("%d %d %d %d %d",&n,&m,&k,&l,&b);
+	printf("Give no of nodes and no of edges\n");
+	scanf("%d %d",&nodeNo,&m);
+//	int * D = malloc(n*sizeof(int));
+	keys = malloc((nodeNo+1)*sizeof(int));
+	place = malloc((nodeNo+1)*sizeof(int));
+	struct node * Q = EmptyQ(nodeNo+1);
 	
-	Insert(Q, 0, 0);
+	Insert(Q, 1, 0);
 
-	for(i=1;i<n;i++) {
-		Insert(Q,1,20001);	
+	for(i=2;i<=nodeNo;i++) {
+		Insert(Q,i,20001);	
 	}
+
+	for (i=1;i<=n;i++) 
+		printf("node %d distance %d\n",Q[i].name, Q[i].distance);
 	
-	int head;
-	struct edge * e; 
-	struct edge * temp1; 
-	struct edge * temp2;
+	int head,tail,distance;
 
-	struct edge * edges = malloc(n*sizeof(struct edge *));
-	for (i=0;i<m;i++) { // δμιουργία λίστας γειτνίασης
-		e = malloc(sizeof(struct edge));
+	struct edge ** edges = calloc(nodeNo,sizeof(struct edge *));
+	
+	for (i=0;i<m;i++) { // δηιουργία λίστας γειτνίασης
+		printf("Give edge head tail length\n");
+		scanf("%d %d %d",&head,&tail,&distance);
+		head++;
+		tail++;
+		insertEdge(edges,head,tail,distance);
+		insertEdge(edges,tail,head,distance);
+		//printf("%d %d %d\n",head,e->tail,e->length);
 		
-		scanf("%d %d %d ",&head,&e->tail,&e->length);
-		e->next = NULL;
-
-		temp1 = &edges[head];
-		temp2 = temp1;
-		while(temp1 != NULL) {
-			temp2 = temp1;
-			temp1 = temp1->next;
-		}
-		temp2 = e;
 	}	
 
-	
+	printf("created list\n");
+	struct edge * temp1;
+
+	printf("nodeNo = %d\n",nodeNo);
+
+		
+	int j;
 	struct node * p;
-	for (i=0;i<n;i++) {
+	for (i=1;i<=nodeNo;i++) {
 		p = ExtractMin(Q);
-		D[p->name] = p->distance;
-		temp1 = &edges[p->name];
+		printf("extracted %d\n",p->name);
+	//	D[p->name] = p->distance;
+		temp1 = edges[p->name];
 		while (temp1!=NULL) {
 			if ((p->distance+temp1->length) < keys[temp1->tail])
-				DecreaseKey(Q, temp1->tail, p->distance + temp1->length);
+				DecreaseKey(Q, place[temp1->tail], p->distance + temp1->length);
+			temp1 = temp1->next;
 		}
 
-		free(p);
+		//free(p);
+		for (j=1;j<=nodeNo;j++) 
+			printf("node %d distance %d\n",Q[j].name, Q[j].distance);
 	}
+	printf("done with the loop\n");
+
+	for(i=1;i<=nodeNo;i++)
+		printf("%d\n",keys[i]);
 
 }
